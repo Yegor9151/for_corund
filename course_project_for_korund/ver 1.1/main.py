a89b9f9fc0d7a06a0b0b482855f547d24b3960a9
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2020-10-15 22:41:55 +0300

    Add files via upload

--- course_project_for_korund/ver 1.1/main.py
+++ course_project_for_korund/ver 1.1/main.py
@@ -5,18 +5,8 @@ from support import date_type
 
 import pandas as pd
 
-# INIT DATABASE
-df = pd.DataFrame(
-    columns=['ФИО', 'Возраст', 'Дата рождения', 'Интересы', 'Дата регистрации']
-)
 
-# INIT MASTER
-root = Tk()
-root.title('Регистрация клиентов')  # rename master window
-
-
-# FUNCTIONS FOR BUTTONS
-def get_data():
+def collect_data():
     """get data and collect it in data base"""
 
     # BASE VARIABLES
@@ -35,92 +25,116 @@ def get_data():
     age = (data['Дата регистрации'] - data['Дата рождения']) / 365.25  # calculate age
     data['Возраст'] = age.days  # days out
 
+    interest_list = flags_data()
+    data['Интересы'] = ', '.join(interest_list)
+
+    global df
+    df = df.append(data, ignore_index=True)
+
+    # clean_data()
+
+
+def clean_data():
+    ent_name.delete(0, END)
+    ent_b_date.delete(0, END)
+    i = 0
+    while i < len(booleans_interest):
+        booleans_interest[i].set(0)
+        i += 1
+
+
+def flags_data():
     interest_list = []
-    booleans_interest = bool_py, bool_ja, bool_1c, bool_an, bool_no
-    interest_name = 'наука', 'техника', 'икусство', 'путешествие', 'другое'
     i = 0
     while i < len(booleans_interest):
         bool_interest = booleans_interest[i]
         if bool_interest.get() == 1:
-            interest_list.append(interest_name[i])
+            interest_list.append(interest_names[i])
         i += 1
 
-    data['Интересы'] = interest_list
-
-    # CLEAN STRINGS
-    ent_name.delete(0, END)
-    ent_b_date.delete(0, END)
-
-    # COLLECT
-    global df
-    df = df.append(data, ignore_index=True)
+    return interest_list
 
 
-def base():
-    base_frame.pack(side=TOP, anchor=W, pady=10)
+def top_open():
+    top_frame.pack(side=TOP, anchor=W, pady=10)
 
-    lab_name.grid(row=0, column=0, sticky=E)
+    lbl_name.grid(row=0, column=0, sticky=E)
     ent_name.grid(row=0, column=1, sticky=W, padx=10)
 
-    lab_b_date.grid(row=1, column=0, sticky=E)
+    lbl_b_date.grid(row=1, column=0, sticky=E)
     ent_b_date.grid(row=1, column=1, sticky=W, padx=10)
 
 
-def language():
-    lang_frame.pack(side=TOP, anchor=W)
+def interest_open():
+    interest_frame.pack(side=TOP, anchor=W)
 
-    lab_languages.pack(side=TOP, anchor=W)
+    interest_lbl.pack(side=TOP, anchor=W)
 
-    flag_py.pack(side=TOP, anchor=W)
-    flag_ja.pack(side=TOP, anchor=W)
-    flag_1c.pack(side=TOP, anchor=W)
-    flag_an.pack(side=TOP, anchor=W)
+    i = 0
+    while i < len(flags):
+        flags[i].pack(side=TOP, anchor=W)
+        i += 1
 
 
-def buttons():
+def buttons_open():
     but_frame.pack(side=TOP, anchor=E)
 
     but_data.pack(side=LEFT, pady=10)
     but_close.pack(side=LEFT, padx=10, pady=10)
 
 
-# BASE DATA
-base_frame = Frame(master=root)
-# STRING NAME
-lab_name = Label(master=base_frame, text='Введите ФИО:')
-ent_name = Entry(master=base_frame, width=40)
-# STRING BIRTH DATE
-lab_b_date = Label(master=base_frame, text='Введите дату рождения:')
-ent_b_date = Entry(master=base_frame, width=40)
-
-# CHOICE LANGUAGE
-lang_frame = Frame(master=root)
-lab_languages = Label(master=lang_frame, text='Выберите языки которые знаете или изучаете:')
-
-
 def create_flag(master, name):
     boolean = BooleanVar()
     boolean.set(0)
     flag = Checkbutton(master=master, text=name, variable=boolean, onvalue=1, offvalue=0)
+
     return flag, boolean
 
 
-# FLAGS OF LANGUAGES
-flag_py, bool_py = create_flag(master=lang_frame, name='наука')
-flag_ja, bool_ja = create_flag(master=lang_frame, name='техника')
-flag_1c, bool_1c = create_flag(master=lang_frame, name='искусство')
-flag_an, bool_an = create_flag(master=lang_frame, name='путешествие')
-flag_no, bool_no = create_flag(master=lang_frame, name='другое')
+# INIT DATABASE
+columns = ['ФИО', 'Возраст', 'Дата рождения', 'Интересы', 'Дата регистрации']
+df = pd.DataFrame(columns=columns)
+
+# INIT MASTER
+root = Tk()
+root.title('Регистрация клиентов')  # rename master window
+
+# BASE DATA
+top_frame = Frame(master=root)
+# STRING NAME
+lbl_name = Label(master=top_frame, text='Введите ФИО:')
+ent_name = Entry(master=top_frame, width=40)
+# STRING BIRTH DATE
+lbl_b_date = Label(master=top_frame, text='Введите дату рождения:')
+ent_b_date = Entry(master=top_frame, width=40)
+
+# CHOICE INTEREST
+interest_frame = Frame(master=root)
+interest_lbl = Label(master=interest_frame, text='Выберите интересы:')
+# INTEREST LIST
+interest_names = 'Наука', 'Техника', 'Икусство', 'Путешествие', 'Спорт', 'Другое'
+# CREATE FLAGS
+flag_sc, bool_sc = create_flag(master=interest_frame, name=interest_names[0])
+flag_tech, bool_tech = create_flag(master=interest_frame, name=interest_names[1])
+flag_art, bool_art = create_flag(master=interest_frame, name=interest_names[2])
+flag_tr, bool_tr = create_flag(master=interest_frame, name=interest_names[3])
+flag_sp, bool_sp = create_flag(master=interest_frame, name=interest_names[4])
+flag_an, bool_an = create_flag(master=interest_frame, name=interest_names[5])
+# PACK FLAGS IN VARIABLE
+flags = flag_sc, flag_tech, flag_art, flag_tr, flag_sp, flag_an
+booleans_interest = bool_sc, bool_tech, bool_art, bool_tr, bool_sp, bool_an
 
 # BUTTON GET DATA
 but_frame = Frame(master=root)
 # COMMANDS
-but_data = Button(master=but_frame, text='Enter', width=10, command=get_data)
+but_data = Button(master=but_frame, text='Enter', width=10, command=collect_data)
 but_close = Button(master=but_frame, text='Close', width=10, command=root.destroy)
 
 # OPEN PEACES
-base()
-language()
-buttons()
+top_open()
+interest_open()
+buttons_open()
+
 # OPEN WINDOWS
 root.mainloop()
+df = df.drop_duplicates(subset=columns[:-2], keep='last')
