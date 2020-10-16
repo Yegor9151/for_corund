dc3723b08474c41cf566f4dba061ea4f1ebd7dbc
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2020-10-16 16:36:10 +0300

    Add files via upload

--- course_project_for_korund/ver 1.1/main.py
+++ course_project_for_korund/ver 1.1/main.py
@@ -62,17 +62,17 @@ def flags_data():
 
 
 def top_open():
-    top_frame.pack(side=TOP, anchor=W, pady=10)
+    top_frame.pack(side=TOP, anchor=W, padx=10, pady=10)
 
     lbl_name.grid(row=0, column=0, sticky=E)
-    ent_name.grid(row=0, column=1, sticky=W, padx=10)
+    ent_name.grid(row=0, column=1, sticky=W)
 
     lbl_b_date.grid(row=1, column=0, sticky=E)
-    ent_b_date.grid(row=1, column=1, sticky=W, padx=10)
+    ent_b_date.grid(row=1, column=1, sticky=W)
 
 
 def radio_open():
-    radio_frame.pack(side=TOP, anchor=W)
+    radio_frame.pack(side=TOP, anchor=W, padx=10)
     lbl_sex.pack(side=TOP, anchor=W)
 
     male.pack(side=LEFT, anchor=W)
@@ -80,7 +80,7 @@ def radio_open():
 
 
 def flags_open():
-    interest_frame.pack(side=TOP, anchor=W, pady=10)
+    interest_frame.pack(side=TOP, anchor=W, padx=10, pady=10)
 
     interest_lbl.pack(side=TOP, anchor=W)
 
@@ -97,11 +97,11 @@ def buttons_open():
     but_close.pack(side=LEFT, padx=10, pady=10)
 
 
-def create_flag(master, name):
+def create_flag(name):
     boolean = BooleanVar()
     boolean.set(0)
-    flag = Checkbutton(master=master, text=name, variable=boolean, onvalue=1, offvalue=0)
-
+    flag = Checkbutton(master=interest_frame, text=name, variable=boolean,
+                       onvalue=1, offvalue=0)
     return flag, boolean
 
 
@@ -138,12 +138,12 @@ interest_lbl = Label(master=interest_frame, text='Выберите интере�
 # INTEREST LIST
 interest_names = 'Наука', 'Техника', 'Икусство', 'Путешествие', 'Спорт', 'Другое'
 # CREATE FLAGS
-flag_sc, bool_sc = create_flag(master=interest_frame, name=interest_names[0])
-flag_tech, bool_tech = create_flag(master=interest_frame, name=interest_names[1])
-flag_art, bool_art = create_flag(master=interest_frame, name=interest_names[2])
-flag_tr, bool_tr = create_flag(master=interest_frame, name=interest_names[3])
-flag_sp, bool_sp = create_flag(master=interest_frame, name=interest_names[4])
-flag_an, bool_an = create_flag(master=interest_frame, name=interest_names[5])
+flag_sc, bool_sc = create_flag(name=interest_names[0])
+flag_tech, bool_tech = create_flag(name=interest_names[1])
+flag_art, bool_art = create_flag(name=interest_names[2])
+flag_tr, bool_tr = create_flag(name=interest_names[3])
+flag_sp, bool_sp = create_flag(name=interest_names[4])
+flag_an, bool_an = create_flag(name=interest_names[5])
 # PACK FLAGS IN VARIABLE
 flags = flag_sc, flag_tech, flag_art, flag_tr, flag_sp, flag_an
 booleans_interest = bool_sc, bool_tech, bool_art, bool_tr, bool_sp, bool_an
@@ -151,8 +151,8 @@ booleans_interest = bool_sc, bool_tech, bool_art, bool_tr, bool_sp, bool_an
 # BUTTON GET DATA
 but_frame = Frame(master=root)
 # COMMANDS
-but_data = Button(master=but_frame, text='Enter', width=10, command=collect_data)
-but_close = Button(master=but_frame, text='Close', width=10, command=root.destroy)
+but_data = Button(master=but_frame, bg='#cbffc3', text='Save', width=10, command=collect_data)
+but_close = Button(master=but_frame, bg='#ffcccb', text='Close', width=10, command=root.destroy)
 
 # OPEN PEACES
 top_open()
