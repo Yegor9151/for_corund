102f794055a1991ff255d323759c46835c8a57e2
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-01-17 03:32:24 +0300

    Add files via upload

--- python/module2/course_project/bot.py
+++ python/module2/course_project/bot.py
@@ -1,20 +1,17 @@
 class Bot:
-    __api_method_url = 'https://api.vk.com/method/'
+    __base_url = 'https://api.vk.com/method/'
     __methods = 'wall.get?'
 
     def __init__(self, owner_id, token):
         self.owner_id = owner_id
         self.token = token
 
-    def create_url(self):
-        count = 100
-        offset = 0
+    def create_url(self, count=100, offset=0):
         parameters = f'owner_id=-{self.owner_id}&' \
                      f'count={count}&' \
                      f'offset={offset}&' \
                      f'access_token={self.token}&' \
                      f'v=5.126'
 
-        full_url = self.__api_method_url + self.__methods + parameters
+        return self.__base_url + self.__methods + parameters
 
-        return full_url
--- python/module2/course_project/main.py
+++ python/module2/course_project/main.py
@@ -1,5 +1,6 @@
-from support import read_file
+from support import *
 from bot import Bot
+from pprint import pprint
 
 # https://oauth.vk.com/authorize?
 # client_id=ID_APP&
@@ -9,10 +10,19 @@ from bot import Bot
 # response_type=token&
 # v=5.52
 
+# ТОКЕН
 path_to_token = '..\\token.txt'
 token = read_file(path_to_token)
 
+# ID ГРУППЫ
 corundum_id = 44273004
 
+# ИНИЦИАЛИЗАЦИЯ БОТА
 corundum_liker = Bot(token=token, owner_id=corundum_id)
-print(corundum_liker.create_url())
+
+# ССЫЛКА НА JSON СТЕНЫ
+wall_url = corundum_liker.create_url(count=1)
+
+# ОТВЕТ СО СТЕНЫ
+result = get_data(wall_url)
+pprint(result)
