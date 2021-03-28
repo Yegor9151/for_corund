139ebae95619654456224961424c2d588551e0ea
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-03-28 14:41:36 +0300

    Delete python/module2 directory

--- python/module2/course_project/support.py
+++
@@ -1,8 +0,0 @@
-def read_file(path) -> str:
-    """ Читает содержимое файла
-    input: path to file
-    output: text """
-
-    with open(path, 'r', encoding='utf-8') as f:
-        text = f.read()
-        return text
