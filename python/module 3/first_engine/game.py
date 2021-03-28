226326bd0fb769a1917410be2698a54ed68460dd
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-03-28 21:48:39 +0300

    Add files via upload

---
+++ python/module 3/first_engine/game.py
@@ -0,0 +1,30 @@
+"""
+Модуль с основным классом-конструктором Game - для создания игры.
+"""
+
+import pygame
+
+
+class Game:
+    """
+    Класс-конструктор, содержит основные методы и переменные для написания игр
+    """
+
+    running = True
+
+    def __init__(self, width=400, height=300):
+        """
+        :parameter width: высота окна
+        :parameter height: ширина окна
+        """
+        self.width = width
+        self.height = height
+
+        self.parentSurface = pygame.display.set_mode(
+            size=(self.width, self.height)
+        )  # родительское окно
+
+    def close(self, event) -> None:
+        if event.type == 256 or (event.type == 768 and event.key == 27):  # если нажал крестик или ESC
+            pygame.quit()  # деинициализируем pygame
+            self.running = False  # отключаем цикл
