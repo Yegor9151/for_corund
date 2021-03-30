145c97c70d303012412018aa5db6ccbc90ac4bb3
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-03-30 16:00:17 +0300

    Add files via upload

--- python/module 3/first_engine/game.py
+++ python/module 3/first_engine/game.py
@@ -11,22 +11,44 @@ class Game:
     """
 
     runner = True
+    clock = pygame.time.Clock()  # счетчик FPS
 
-    def __init__(self, width=400, height=300):
+    def __init__(self, width=400, height=300, color=(0, 0, 0)):
         """
         :parameter width: высота окна
         :parameter height: ширина окна
         """
         self.width = width
         self.height = height
+        self.colorSurface = color
 
         self.parentSurface = pygame.display.set_mode(
-            size=(self.width, self.height)
+            size=(width, height)
         )  # родительское окно
+        self.parentSurface.fill(color)
+
+    @staticmethod
+    def display_update():
+        pygame.display.update()
 
     @staticmethod
     def events():
         return pygame.event.get()
+
+    def window_fill(self):
+        """
+        Метод при вызове которого заливается родительское окно
+        :return: None
+        """
+        self.parentSurface.fill(self.colorSurface)  # заливаем родительское окно
+
+    def fps_counter(self, FPS=30) -> None:
+        """
+        Метод уравляущий частотой обновления кадров
+        :param FPS: число кадров в секунду
+        :return: None
+        """
+        self.clock.tick(FPS)
 
     def close(self, event) -> None:
         if event.type == 256 or (event.type == 768 and event.key == 27):  # если нажал крестик или ESC
