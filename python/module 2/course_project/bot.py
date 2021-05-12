5463956d8511d9df54667f0da7b52294b558c56b
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-05-12 20:05:10 +0300

    Add files via upload

--- python/module 2/course_project/bot.py
+++ python/module 2/course_project/bot.py
@@ -1,30 +1,29 @@
-from pprint import pprint
 import requests
 
 
 class Bot:
-    __base_url = 'https://api.vk.com/method/'
-    __methods = {'wall_get': 'wall.get?',
-                 'likes_add': 'likes.add?'}
+    base_url = 'https://api.vk.com/method/'
+    methods = {'wall_get': 'wall.get?',
+               'likes_add': 'likes.add?'}
 
-    __full_url = None  # заполнится поле create_url
-    __data_list = []  # здесь храним информацию о постах
+    full_url = None  # заполнится поле create_url
+    data_list = []  # здесь храним информацию о постах
 
-    def __init__(self, group_id, token):
-        self.__base_params = f'owner_id=-{group_id}&' \
-                             f'access_token={token}&' \
-                             f'v=5.21&'
+    def __init__(self, owner_id, token):
+        self.base_params = f'owner_id=-{owner_id}&' \
+                           f'access_token={token}&' \
+                           f'v=5.21&'
 
     def create_url(self, count=1, offset=0):
-        self.__full_url = self.__base_url + \
-                          self.__methods['wall_get'] + \
-                          self.__base_params + \
-                          f'count={count}&' \
-                          f'offset={offset}&'
-        return self.__full_url
+        self.full_url = self.base_url + \
+                        self.methods['wall_get'] + \
+                        self.base_params + \
+                        f'count={count}&' \
+                        f'offset={offset}&'
+        return self.full_url
 
     def get_data(self):
-        data = requests.get(self.__full_url)
+        data = requests.get(self.full_url)
         data = data.json()
         try:
             data = data['response']['items']
@@ -32,21 +31,21 @@ class Bot:
                 data_dict = {'text': post['text'],
                              'id': post['id'],
                              'like': post['likes']['user_likes']}
-                self.__data_list.append(data_dict)
-            return self.__data_list
+                self.data_list.append(data_dict)
+            return self.data_list
         except KeyError:
-            return data['error']['error_msg']
+            error = data['error']['error_msg']
+            return error
 
     def add_likes(self):
         count = 0  # счетчик лайков
-
-        for post in self.__data_list:
+        for post in self.data_list:
             if post['like']:
                 print(f"Для {post['id']} лайк уже был поставлен")
             else:
-                url = self.__base_url + \
-                      self.__methods['likes_add'] + \
-                      self.__base_params + \
+                url = self.base_url + \
+                      self.methods['likes_add'] + \
+                      self.base_params + \
                       f"type=post&" \
                       f"item_id={post['id']}&"
 
--- python/module 2/course_project/main.py
+++ python/module 2/course_project/main.py
@@ -3,25 +3,31 @@ from bot import Bot  # импортируем класс-конструктор 
 from pprint import pprint
 
 
-class BotLiker(Bot):  # делаем нашего бота наследником класса Bot
-    def __init__(self, group_id, path_to_token):  # метод конструктор
-        # обращаемся к методу конструктору родительского класса
-        super().__init__(group_id, token=read_file(path_to_token))
+class BotLiker(Bot):
+    def __init__(self, owner_id, path_to_token):
+        super().__init__(owner_id, token=read_file(path_to_token))
 
-    def run(self, count=1):
-        likes = 0  # считаем число лайков
-        # запускаем цикл в условии которого стоит число пакетов от заявленного числа постов
-        for offset in (range(count // 100) if count % 100 == 0 else range((count // 100) + 1)):
-            url = self.create_url(count=count, offset=offset * 100)  # бот - создай ссылку
+    def run(self, count=1):  # pipline
+        """
+        Метод для выполнения последовательности действий
+        """
+        offsets = range(count // 100) if count % 100 == 0 else range((count // 100) + 1)
+
+        for offset in offsets:
+            count_now = 100 if offset != offsets[-1] else count - (offsets[-1] * 100)
+
+            url = self.create_url(count=count_now, offset=offset * 100)  # бот - создай ссылку
             data = self.get_data()  # бот - достань данные
-            likes += self.add_likes()  # бот - поставь лайки
 
-            # эта часть для разработчика
             print(f'\nСсылка до постов:\n{url}')
+
             if type(data) == str:
-                print(f'\nПроизошла ошибка:\n{data}')
+                print(data)
             else:
                 print(f'\nСостояние данных:\n')
                 pprint(data)
 
-        return likes  # возвращаем число лайков
+        count_like = self.add_likes()  # бот - поставь лайки
+
+        print(f'\nЧисло проставленных лайков {count_like}')
+        return count_like
