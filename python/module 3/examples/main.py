54e2b541625d60202a38e72616f59afb5c6c940e
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-06-13 13:50:12 +0300

    Add files via upload

--- python/module 3/examples/main.py
+++ python/module 3/examples/main.py
@@ -1,17 +1,14 @@
 from first_engine.game import Game
 from first_engine import objects
-import pygame
 
 
 class FirstGame(Game):
     def run(self):
         hero = objects.Character(self.surface, speed=8)
-        hero.load_sprites(name='stand_right', path='./sprites/viking/stand/right')
-        hero.load_sprites(name='stand_left', path='./sprites/viking/stand/left')
-        hero.load_sprites(name='run_left', path='./sprites/viking/run/left')
-        hero.load_sprites(name='run_right', path='./sprites/viking/run/right')
-
-        wall = objects.Barrier(self.surface, objects=[hero], width=200, height=50, x=300, y=450)
+        hero.load_sprites(name='stand_right', path='./sprites/viking/stand/right/')
+        hero.load_sprites(name='stand_left', path='./sprites/viking/stand/left/')
+        hero.load_sprites(name='run_left', path='./sprites/viking/run/left/')
+        hero.load_sprites(name='run_right', path='./sprites/viking/run/right/')
 
         game_over = False
         while self.RUNNER:
@@ -20,24 +17,11 @@ class FirstGame(Game):
 
                 # Объекты
                 hero.blit()
-                wall.blit()
+                hero.play_animation()
+                hero.motion_left()
+                hero.motion_right()
 
-                # проверяем - уперся ли персонаж в преграду
-                self.window_borders([wall, hero])
-                wall.resistance()
-
-                # Движения персонажа
-                hero.motion_left(sprites_active='run_left', sprite_inactive='stand_left', time_to_update=6)
-                hero.motion_right(sprites_active='run_right', sprite_inactive='stand_right', time_to_update=6)
-                # роняем персонажа
-                hero.drop(speed_up=1, max_speed=20)
-
-                # проверяем - уперся ли персонаж в преграду
                 self.window_borders([hero])
-                wall.resistance()
-
-                # если да, то даем ему возможность прыгать
-                hero.action_jump()
 
             for event in self.events():
                 # print(event)  # отслеживание событий
