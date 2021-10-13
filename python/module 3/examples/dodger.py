8a4348741078ddb26bae0000b04e5e8ae4a0daa6
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2021-10-13 19:28:18 +0300

    Add files via upload

--- python/module 3/examples/dodger.py
+++ python/module 3/examples/dodger.py
@@ -1,69 +1,53 @@
 from first_engine.game import Game
+from first_engine.characters import Character
+from first_engine.borders import Border
 from first_engine.objects import Object
 import random
 
 
-class FirstGame(Game):
+class MyGame(Game):
 
-    def run(self):
-        RED = (255, 200, 200)
-        YELLOW = (255, 255, 200)
-        GREEN = (200, 255, 200)
-        BLUE = (200, 200, 255)
+    def run(self):  # рабочая область
 
-        colors = BLUE, GREEN, YELLOW, RED
-
-        game_over = False
-
-        hero_width, hero_height = 40, 40
-        start_position_x = self.body.width // 2 - hero_width // 2
-        start_position_y = self.body.height - hero_height
-
-        hero = Object(
-            parent=self.surface, speed=4,
-            width=hero_width, height=hero_height,
-            x=start_position_x, y=start_position_y
-        )
+        start_posX = self.w // 2 - 20
+        start_posY = self.h - 40
+        char = Character(width=40, height=40,
+                         x=start_posX, y=start_posY,
+                         speed=4)
         enemies = []
+        game_over = False
+        time_to_spawn = 60
 
-        timer = 60
-        while self.RUNNER:
+        while True:
             if not game_over:
-                self.fps_counter(60)
-                self.window_borders(objects=[hero])
+                self.fps_control()
                 self.display_update()
-                self.window_fill()
+                self.surface.fill((0, 0, 0))
 
-                hero.blit()
-                hero.motion_control()
+                self.resistance(objs=[char])
 
-                timer -= 1
-                if timer == 0:
-                    enemy_speed = random.randint(1, 4)
-                    enemies.append(Object(
-                        parent=self.surface, speed=enemy_speed, color=colors[enemy_speed - 1],
-                        x=random.randint(0, self.body.width - hero_width), y=-40
-                    ))
-                    timer = 60
+                char.blit(self.surface)
+                char.motion()
+
+                time_to_spawn -= 1
+                if time_to_spawn == 0:
+                    enemies.append(Object(x=random.randint(0, self.w - 40), y=-40))
+                    time_to_spawn = 60
 
                 for enemy in enemies:
-                    enemy.blit()
-                    enemy.y += enemy.speed
-                    enemy.replace()
-                    if enemy.body.colliderect(hero.body):
+                    enemy.blit(self.surface)
+                    enemy.rect.y += 2
+                    if enemy.rect.colliderect(char.rect):
                         game_over = True
-                    if enemy.body.top == self.body.bottom:
+                    if enemy.rect.top == self.surface.get_rect().bottom:
                         enemies.remove(enemy)
 
-            for event in self.events():
-                # print(event)  # отслеживание событий
-
-                if event.type == 768 and event.key == 114:  # при нажатии на R игра начинается заново
-                    hero.replace(x=start_position_x, y=start_position_y)  # обновляем позицию героя
-                    enemies.clear()  # отчищаем список врагов
-                    game_over = False  # отключаем конец игры
-
+            for event in self.get_events():
                 self.close(event)
+                if event.type == 768 and event.key == 114:  # R
+                    char.rect.x, char.rect.y = start_posX, start_posY
+                    enemies.clear()
+                    game_over = False
 
 
-FirstGame(width=800, height=800).run()
+MyGame().run()
