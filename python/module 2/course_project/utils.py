11d685e4028fe316ee97f7ef7c8a497d95aecf08
Sorokin Yegor <56124955+Yegor9151@users.noreply.github.com>
2023-01-17 12:02:34 +0300

    Add files via upload

--- python/module 2/course_project/utils.py
+++ python/module 2/course_project/utils.py
@@ -1,35 +1,34 @@
-"""
-В этом модуле хранятся удобные утилиты
-"""
+def get_token(client_id, scope="wall,offline"):
+    """Функция для получения токена
 
-
-def generate_token(client_id):
-    """
-    Эта фунция создает ссылку до токена
-
-    :param client_id: id приложения в vk.com
-    :return: ссылка
+    :param client_id: id приложения в vk
+    :param scope: расширения для токена
+    :return: ссылка на страницу в адресе которой будет токен
     """
     url = f"https://oauth.vk.com/authorize?" \
           f"client_id={client_id}&" \
           f"display=page&" \
           f"redirect_uri=https://oauth.vk.com/blank.html&" \
-          f"scope=wall,offline&" \
+          f"scope={scope}&" \
           f"response_type=token&" \
-          f"v=5.81&"
-
+          f"v=5.131"
     return url
 
+# print(get_token(12345678))
 
-def read_token(path):
-    """
-    Функция для чтения токена (файла)
+def open_file(path, mode='r', text=None, encoding="utf-8"):
+    """Читает или запысывает файл
 
-    :param path: путь до файла
-    :return: содержимое файла
+    :param path: str - путь до файла
+    :param mode: char = 'r' - читать (r) или записать (w)
+    :param text: str - если на запись, то можно написать текст для записи
+    :param encoding: str - в какой кадировке читать или записывать файл
+    :return: str - тект файла
     """
-    try:
-        with open(file=path, mode="r", encoding="utf-8") as file:
-            return file.read()
-    except FileNotFoundError as e:
-        print(f"Путь не найден: {e}")
+    with open(path, mode=mode, encoding=encoding) as f:
+        if mode == 'r':
+            return f.read()
+        elif mode == 'w':
+            f.write(text)
+            return text
+
